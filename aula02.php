c94c0f742dc179d8b433cd662800b43019cc5d96
Kauã <kauanasc2008@gmail.com>
2026-04-08 15:56:33 -0300

    Adicionando aulas de PHP

---
+++ aula02.php
@@ -0,0 +1,73 @@
+
+<!-- 08/04/26 - Quarta -->
+
+<?php
+// echo "Programando PHP - 08/04/2026 <br>";
+// echo "A minha turma é TI101".'<br>';
+// echo nl2br("Teste \n");
+
+// // \n - só funciona no console
+// echo nl2br("Aqui quebra a linha no \n HTML"); 
+// // nl2br permite que quebre a linha no html
+
+
+// $valor; //Declaração
+// $valor = 10; 
+
+// $n1 = "1";
+// $n2 = ;
+
+//$resultado = ($n1+$n2);
+
+// ===  Valor igual e mesmo tipo
+// ==   Valor igual
+
+// if($resultado == 2){
+//     // Se a comparação for verdadeira
+//     echo ("O valor é = ".$resultado);
+// }
+
+// else{
+//  echo ("O valor é falso");
+// }
+
+
+
+// $n1 = 1;
+// $n2 = 1;
+
+// if($n1 == $n2){
+//     // Se a comparação for verdadeira
+//     echo ("O valor é igual");
+// }
+
+// else{
+//  echo ("Diferente");
+// }
+
+/*
+is_int()
+is_float()
+is_string()
+is_boolean()
+is_array()
+is_null()
+*/
+
+
+
+$n1 = 1;
+
+if(is_int($n1)){
+    // Se a comparação for verdadeira
+    echo ("O valor é int");
+}
+
+else{
+ echo ("O valor NÃO é inteiro");
+}
+
+
+
+
+?>
---
+++ linguagem_aula01.php
@@ -0,0 +1,132 @@
+<?php
+
+echo "<h1>Programando com PHP</h1>";
+
+// Comentário de uma linha
+
+/*
+Comentário de
+várias linhas
+linhas
+linhas
+*/
+// echo "Testando PHP";
+// $nome = "Kauã";
+// echo $nome;
+
+
+/*
+$valor = 10;                // inteiro
+$nome = "Fernando Diniz";   //string
+$preco = 200.00;             //Decimal
+
+echo ("Valores da variáveis ="." ".$valor." ".$nome." ".$preco);
+*/
+
+// ====================================================================================
+
+/*
+// VARIÁVEIS
+$verdadeiro = true;
+$falso = false;
+$inteiro = "1";
+$decimal = 1.5;
+$nome = "Kauã";
+$frase = "PHP é uma linguagem para WEB";
+$separador = "****************";
+
+// BREAK ROW
+echo $verdadeiro.'<br>';
+echo $falso.'<br>';
+echo $inteiro.'<br>';
+echo $decimal.'<br>';
+echo $nome.'<br>';
+echo $frase.'<br>';
+echo $separador.'<br>';
+
+// GETTYPE ()
+echo gettype ($verdadeiro).'<br>';
+echo gettype ($falso).'<br>';
+echo gettype ($inteiro).'<br>';
+echo gettype ($decimal).'<br>';
+echo gettype ($nome).'<br>';
+echo gettype ($frase).'<br>';
+echo gettype ($separador).'<br>';
+*/
+
+// ====================================================================================
+
+/*
+$valor = null;
+$valor = 0;
+$valor = "Zero";
+$valor = false;
+*/
+
+// ====================================================================================
+/*
+
+// Operções
+//Soma 
+$n1 = 10;
+$n2 = 20;
+$resultado1 = $n1+$n2;
+echo ("O valor da Soma é ="." ".$resultado1.'<br>');
+
+//Subtração
+$num1 = 10;
+$num2 = 20;
+$resultado2 = $num1-$num2;
+echo ("O valor da Subtração é ="." ".$resultado2.'<br>');
+
+//Multiplicação
+$nume1 = 10;
+$nume2 = 20;
+$resultado3 = $nume1*$nume2;
+echo ("O valor da Mutiplicação é ="." ".$resultado3.'<br>');
+
+//Divisão
+$numero1 = 10;
+$numero2 = 20;
+$resultado4 = $numero1/$numero2;
+echo ("O valor da Divisão é ="." ".$resultado4.'<br>');
+
+
+//Resto da Divisão
+$numero10 = 10;
+$numero20 = 20;
+$resultado5 = $numero10%$numero20;
+echo ("O valor do Resto da Divisão é ="." ".$resultado5.'<br>');
+
+//Potenciação
+$numero100 = 10;
+$numero200 = 20;
+$resultado6 = $numero100**$numero200;
+echo ("O valor da Potenciação é ="." ".$resultado5.'<br>');
+*/
+
+
+// ====================================================================================
+
+/*
+// Ordem de Precedência:
+//Precedência = () > ** / % * > + -
+echo 2+2*5+(9/3)/(4+2**3);
+*/
+
+// ====================================================================================
+
+// INTERPOLAÇÃO
+/*
+$nome = "Kauã";
+echo 'olá, $nome'.'<br>';
+echo "O valor da variável '$nome' é = {$nome}";
+*/
+
+// ====================================================================================
+
+
+
+// ====================================================================================
+
+?>
