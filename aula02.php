4ad04672cf6e5251cf7dd1ac9d123a37b3ef556c
Kauã <kauanasc2008@gmail.com>
2026-04-08 17:06:42 -0300

    Aprendendo Array

--- aula02.php
+++ aula02.php
@@ -1,6 +1,8 @@
 
 <!-- 08/04/26 - Quarta -->
-
+<!--    IF, ELSE / ARRAY -->
+<!--    IF, ELSE / ARRAY -->
+<!--    IF, ELSE / ARRAY -->
 <?php
 // echo "Programando PHP - 08/04/2026 <br>";
 // echo "A minha turma é TI101".'<br>';
@@ -16,6 +18,8 @@
 
 // $n1 = "1";
 // $n2 = ;
+
+//================================================ IF ELSE ================================================
 
 //$resultado = ($n1+$n2);
 
@@ -56,16 +60,52 @@ is_null()
 
 
 
-$n1 = 1;
+// $n1 = 1;
 
-if(is_int($n1)){
-    // Se a comparação for verdadeira
-    echo ("O valor é int");
-}
+// if(is_int($n1)){
+//     // Se a comparação for verdadeira
+//     echo ("O valor é int");
+// }
 
-else{
- echo ("O valor NÃO é inteiro");
-}
+// else{
+//  echo ("O valor NÃO é inteiro");
+// }
+
+// $n1 = 10;
+// $n2 = "10";
+
+// echo ($n1==$n2);
+
+// ================================================ ARRAY ================================================
+
+// // $nomes = ["João", "Maria", "Pedro", "Kauã"];
+// // $recebe = $nomes[4];
+// // echo $recebe;
+
+//     //Outra maneira 
+// $frutas = array ("Maçã", "Laranja", "Banana");
+//     // Formas de impressão
+// // echo ($frutas[1]."<br>");
+// // print_r ($frutas);
+// // var_dump($frutas);
+
+// $cestaFrutas = $frutas;
+// print_r($cestaFrutas);
+
+//================================================
+
+/*
+$colegas = array ("Diogo","Vinicius Martinez","Julia","Nicolas","Guilherme França","Guilherme Souza","Gabriel","Leonardo","Vinicius Siqueira","Ryan","Guilherme Silva");
+// Adcionando ao Início
+  array_unshift($colegas, "Kauã");
+// Adicionando no Final
+array_push($colegas, "Wilton");
+  $colegas[] = "Well";
+
+echo "<pre>";      //Formatação de Exibição
+print_r($colegas);
+echo "</pre>";
+*/
 
 
 
